3e86dce9804746cd1694b111e66980162ced7ac1
celio001 <pessoalcelio01@gmail.com>
2025-01-19 22:02:09 -0300

    feat: update hendler structure to separate package

--- router/routes.go
+++ router/routes.go
@@ -3,40 +3,22 @@ package router
 import (
 	"net/http"
 
+	"github.com/celio001/api-golang-opportunities.git/handler"
 	"github.com/gin-gonic/gin"
 )
 
 func inicializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
-	{	
+	{
 		//Show Opening
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-		//Show Opening
-		v1.POST("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-		//Show Opening
-		v1.DELETE("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-		//Show Opening
-		v1.PUT("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Opening",
-			})
-		})
-		v1.GET("/opening", func(ctx *gin.Context){
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openings",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		//POST Opening
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		//Delete Opening
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		//Update Opening
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		//Update Openings
+		v1.GET("/opening", handler.ListOpeningsHandler)
 	}
 }
