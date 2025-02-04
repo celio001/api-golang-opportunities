831eecf38959b64ad9874beeef8977a120b67cad
celio001 <pessoalcelio01@gmail.com>
2025-02-04 16:02:11 -0300

    feat: schema DB

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/celio001/api-golang-opportunities.git/router"
+import (
+	"fmt"
+
+	"github.com/celio001/api-golang-opportunities.git/config"
+	"github.com/celio001/api-golang-opportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Inicialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config inicialization error: %v", err)
+		fmt.Println(err)
+	}
 	// Inicialize Router
 	router.Initialize()
 }
